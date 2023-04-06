c3d9ec9527cf8d8238d5910f206c17e2d5f6d87b
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-06 03:38:38 +0300

    feat: pages added

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Welcome from "../views/Welcome.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "welcome",
+    component: Welcome,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/chatroom",
+    name: "chatroom",
+
+    component: () => import("../views/Chatroom.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
