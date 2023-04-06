24a2c16f641a5ef207d33e77128c9320a3089bb8
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-07 01:02:08 +0300

    getting instant data from the firebase is complet

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow/>
     <NewChatFormVue/>
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Navbar from "@/components/Navbar.vue";
 import getUser from "@/composables/getUser";
+
 import { useRouter } from "vue-router";
 import { watch } from "vue";
 import NewChatFormVue from '@/components/NewChatForm.vue';
+import ChatWindow from "@/components/ChatWindow.vue";
 
 export default {
-  components: { Navbar,NewChatFormVue },
+  components: { Navbar,NewChatFormVue,ChatWindow },
   setup() {
     const { user } = getUser();
     const router = useRouter();
